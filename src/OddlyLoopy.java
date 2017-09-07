9c6ca16fff04743b996850dd6842b85ecb3c36b1
League <League@AdministorsiMac.attlocal.net>
2017-09-06 18:36:47 -0700

    Fizz Buzz

---
+++ src/OddlyLoopy.java
@@ -0,0 +1,4 @@
+
+public class OddlyLoopy {
+
+}
--- src/RobotGraffiti.java
+++
@@ -1,9 +0,0 @@
-import org.jointheleague.graphical.robot.Robot;
-
-public class RobotGraffiti {
-	public static void main(String[] args) {
-	Robot r2d2;	
-	
- new Robot = r2d2;
-}
-}
