51e190cab83022d68b810334930665f8ca06eb5f
lehrerbrian <league@WTS21.attlocal.net>
2017-10-18 18:24:56 -0700

    Badger, MyAges, Mustashes2

---
+++ src/Badger.java
@@ -0,0 +1,45 @@
+import javax.swing.JOptionPane;
+
+public class Badger {
+public static void main(String[] args) {
+	
+	for (int i = 0; i < 100 ; i++) {
+		
+	
+	
+	String Badge = "";
+	 for (int s = 0; s < 16; s++) {
+		Badge+="Badger ";
+	}
+	 System.out.println(Badge);
+	 
+	 
+	 
+	 String Mush = "";
+	 for (int a = 0; a < 2; a++) {
+		Mush+="Mushroom ";
+	}
+	 System.out.println(Mush);
+	 
+	 
+	 
+	 String Snak = "";
+	 for (int l = 0; l < 3; l++) {
+		Snak+="Snake ";
+	}
+	 System.out.println(Snak);
+ 
+	}
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+	 
+ 
+}
+}
---
+++ src/MyAges.java
@@ -0,0 +1,23 @@
+import javax.swing.JOptionPane;
+
+public class MyAges {
+public static void main(String[] args) {
+	for (int i = 0; i < 11; i++) {
+	System.out.println(i);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
