1d59319efac5a372eec72304d1b397b331bf8f96
themasterofiron <owendouglas0528>
2017-11-08 18:33:35 -0800

    happyunbday

--- src/HappyUnbirthday.java
+++ src/HappyUnbirthday.java
@@ -1,4 +1,35 @@
+import javax.swing.JOptionPane;
 
 public class HappyUnbirthday {
-
+public static void main(String[] args) {
+	
+	String Bday = JOptionPane.showInputDialog("When is ur B-Day");
+	if (Bday.equals("11-8")) {
+		JOptionPane.showMessageDialog(null, "Happy B-Day");
+		
+	}else { 
+		JOptionPane.showMessageDialog(null, "Merry UnB-Day");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
 }
