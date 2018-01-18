41dc379545ed4f60643823cc3abff6dfee49293a
varnit05 <league@WTS14.attlocal.net>
2018-01-17 18:27:21 -0800

    99 bottles of beer
    fly

--- src/L99BottlesOfBeerOnTheWall.java
+++ src/L99BottlesOfBeerOnTheWall.java
@@ -1,10 +1,13 @@
+
 public class L99BottlesOfBeerOnTheWall {
 	public static void main(String[] args) {
 
-		// print out 99 bottles of beer on the wall and so on down to 1 bottle of beer on the wall
+		// print out 99 bottles of beer on the wall and so on down to 1 bottle of beer
+		// on the wall
 		for (int i = 99; i > 0; i--) {
 
-			System.out.println(i + " bottles of beer on the wall " + i + " bottles of beer, take one down pass it around");
+			System.out.println(
+					i + " bottles of beer on the wall " + i + " bottles of beer, take one down pass it around");
 		}
 	}
 }
